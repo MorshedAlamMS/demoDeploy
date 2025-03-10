a02622f23904c9655cae156567f67acf35aa4802
AmdadulRimon <miamdadulhq@gmail.com>
2025-03-10 11:59:38 +0600

    chore: export content data from one file

---
+++ app/ContentData/index.ts
@@ -0,0 +1,29 @@
+// export About Me Content
+export * from './AboutMe/AboutMeContent';
+
+// export Client Reviews Content
+export * from './ClientsReviews/ClientsReviewsContent';
+
+// export Contact Me Content
+export * from './ContactMe/ContactMeContent';
+
+// export FAQ Content
+export * from './FAQ/FAQContent';
+
+// export Hero Section Content
+export * from './HeroSection/HeroSectionContent';
+
+// export My Client Content
+export * from './MyClients/MyClientsContent';
+
+// export My Strategy Section Content
+export * from './MyStrategySection/MyStrategySectionContent';
+
+// export My Works Process Content
+export * from './MyWorkProcess/MyWorkProcessContent';
+
+// export My Works Content
+export * from './MyWorks/MyWorksContent';
+
+// export Social Link Content 
+export * from './SocialLinks/SocialLinksContent';
