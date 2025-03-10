43a527f9c06834cfd95237f29a53879ab148bdbc
sajibms <shahadath.sajib.ms@gmail.com>
2025-03-10 12:27:41 +0600

    fix: fixed navigation in navbar to scroll to right section

--- app/components/section/home/Portfolio.tsx
+++ app/components/section/home/Portfolio.tsx
@@ -6,12 +6,14 @@ import { clientsWork } from "~/constants";
 
 const Portfolio = () => {
   return (
-    <section id="works" className="lg:pt-[100px] pt-[90px] pb-[60px]">
-      <SectionHeading
-        title="Works I did for my clients"
-        sectionName="Portfolio"
-        titleClass="lg:max-w-[349px] md:max-w-[216px] max-w-[180px]"
-      />
+    <section className="lg:pt-[100px] pt-[90px] pb-[60px]">
+      <div id="works">
+        <SectionHeading
+          title="Works I did for my clients"
+          sectionName="Portfolio"
+          titleClass="lg:max-w-[349px] md:max-w-[216px] max-w-[180px]"
+        />
+      </div>
       <div className="lg:mt-[80px] mt-[50px]">
         <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
           <Marquee pauseOnHover className="[--duration:20s]">
@@ -19,7 +21,11 @@ const Portfolio = () => {
               <ClientWorkCard key={index} {...client} />
             ))}
           </Marquee>
-          <Marquee reverse pauseOnHover className="[--duration:20s] lg:mt-[60px] mt-1">
+          <Marquee
+            reverse
+            pauseOnHover
+            className="[--duration:20s] lg:mt-[60px] mt-1"
+          >
             {clientsWork.map((client, index) => (
               <ClientWorkCard key={index} {...client} />
             ))}
@@ -28,7 +34,12 @@ const Portfolio = () => {
           <div className="pointer-events-none absolute inset-y-0 right-0 w-1/4 bg-gradient-to-l from-background lg:flex hidden"></div>
         </div>
         {/* show all button */}
-        <Link to="/portfolio" className="w-fit flex flex-col items-center justify-center mx-auto lg:mt-[80px] mt-[50px] px-5 h-[46px] font-bold font-gabarito bg-brand-parrot text-primary-text rounded-[10px] hover:rounded-xl text-base transition-all duration-200 ease-in transform hover:scale-95">Show All</Link>
+        <Link
+          to="/portfolio"
+          className="w-fit flex flex-col items-center justify-center mx-auto lg:mt-[80px] mt-[50px] px-5 h-[46px] font-bold font-gabarito bg-brand-parrot text-primary-text rounded-[10px] hover:rounded-xl text-base transition-all duration-200 ease-in transform hover:scale-95"
+        >
+          Show All
+        </Link>
       </div>
     </section>
   );
