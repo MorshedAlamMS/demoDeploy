b0e4f0373872cca576f9e959ea3bab9f650bcf92
sajibms <shahadath.sajib.ms@gmail.com>
2025-03-10 14:31:09 +0600

    docs: added guide to change content of about me section

--- app/ContentData/AboutMe/AboutMeContent.ts
+++ app/ContentData/AboutMe/AboutMeContent.ts
@@ -1,8 +1,22 @@
-const firstParagraph =
-  "Hey, I’m Thumbnail Boss, the guy who designs thumbnails that make people stop, look, and click.";
-const secondParagraph =
-  "I’ve worked with YouTube creators to create scroll-stopping thumbnails. My goal? To make your content impossible to ignore.";
+// This is the variable file for the AboutMe page.
+// -----------------------------------------------------------------------------
+// To update the content of the AboutMe page, simply change the values of the
+// variables below. Please follow these guidelines:
+//
+// *1. Text Content:* ✍️
+//    - The variables 'firstParagraph' and 'secondParagraph' hold the main text.
+//    - *Tip:*  // * Try to keep a similar length with the provided text to preserve the design.
+//
+// *2. Image:* 🖼️
+//    - To change the image on the AboutMe page, update the 'imageName' variable.
+//    - Replace the file in the path: /public/AboutMeImages/
+//    - *Note:*  // * Ensure the image is square and that the file name matches the variable value.
+// -----------------------------------------------------------------------------
 
-export const aboutMeContent = [firstParagraph, secondParagraph];
+export const firstParagraph =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque sed urna. Mauris vitae. Nulla.";
 
-export const aboutMeImageName = "aboutme.png";
+export const secondParagraph =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum. Integer non libero. Donec.";
+
+export const imageName = "aboutme.png";
