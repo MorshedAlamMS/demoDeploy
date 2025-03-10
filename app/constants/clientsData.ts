69574a8cce403f99ac118d0a8b5e639ba8a09994
AmdadulRimon <miamdadulhq@gmail.com>
2025-03-10 16:17:14 +0600

    chore: testimoniels section added

--- app/constants/clientsData.ts
+++ app/constants/clientsData.ts
@@ -1,3 +1,4 @@
+import { clientNames, clientImages, reviewsOfClient } from "~/ContentData";
 import { IClientInfo, IClientReview } from "~/types/client";
 
 export const clients: IClientInfo[] = [
@@ -49,162 +50,19 @@ export const clients: IClientInfo[] = [
 
 ];
 
-export const clientReviewFirstCol = [
-  {
-    name: "YT orbit",
-    subs: "",
-    review: "Sup 🔥",
-    clientImage: "/clientReview/ytorbit.png",
-  },
-  {
-    name: "Ease media",
-    subs: "",
-    review: "This is some good work",
-    clientImage: "/clientReview/ease.png",
-  },
-  {
-    name: "Muskonomy",
-    subs: "",
-    review: "Let's keep up the great work.Your communication and design skills are excellent. I'm hoping for a long-term collaboration here.",
-    clientImage: "/clientReview/muskonomy.png",
-  },
-  {
-    name: "ZHC",
-    subs: "",
-    review: "Great guy, even better thumbnails.",
-    clientImage: "/clientReview/zhc.png",
-  },
-]
 
-export const clientReviewSecondCol = [
-  {
-    name: "James Charles",
-    subs: "",
-    review: "I’m great!!! I came across your thumbnail work, you’re amazing!",
-    clientImage: "/clientReview/james.png",
-  },
-  {
-    name: "Shawn Herrin",
-    subs: "",
-    review: "Bottom right. No idea why, the contrast grabs my eye. When I expanded the image they were all so good. But at small size the white shirt got me.",
-    clientImage: "/clientReview/shawn.png",
-  }, 
-  {
-    name: "Saima",
-    subs: "",
-    review: "Awesome, after seeing this Jimmy would be motivated to go the gym",
-    clientImage: "/clientReview/saima.png",
-  },
-  {
-    name: "Justin Pugh",
-    subs: "",
-    review: "Super easy to work with and crazy talented. Thumbnail BOSS always takes our ideas and levels them up!",
-    clientImage: "/clientReview/justinpugh.png",
-  },
-]
 
-export const clientReviewThirdCol = [
-  {
-    name: "Blue Thunder",
-    subs: "",
-    review: "Brilliant thumbnail designer—his work is clean, impactful, and optimized for maximum engagement!",
-    clientImage: "/clientReview/blueThunder.png",
-  },
-  {
-    name: "ZHC Crafts",
-    subs: "",
-    review: "Fast, reliable, and always on point! ",
-    clientImage: "/clientReview/zhcCrafts.png",
-  },
-  {
-    name: "Ayan Ghosh | Video Editor",
-    subs: "",
-    review: "You cooked 🔥",
-    clientImage: "/clientReview/ayan.png",
-  },
-  {
-    name: "Kamahley",
-    subs: "",
-    review: "Extremely communicative and always focused on making the best possible design.",
-    clientImage: "/clientReview/Kamahley.png",
-  },
-]
+export const clientsReviews: IClientReview[] = 
+reviewsOfClient.map((review,index) => ({
+  name: clientNames[index],
+  subs: "",
+  review: review,
+  clientImage: `/ClientReviewImages/${clientImages[index]}`,
+}))
 
-export const clientsReviews: IClientReview[] = [
-  {
-    name: "YT orbit",
-    subs: "",
-    review: "Sup 🔥",
-    clientImage: "/clientReview/ytorbit.png",
-  },
-  {
-    name: "Ease media",
-    subs: "",
-    review: "This is some good work",
-    clientImage: "/clientReview/ease.png",
-  },
-  {
-    name: "Muskonomy",
-    subs: "",
-    review: "Let's keep up the great work.Your communication and design skills are excellent. I'm hoping for a long-term collaboration here.",
-    clientImage: "/clientReview/muskonomy.png",
-  },
-  {
-    name: "Ayan Ghosh | Video Editor",
-    subs: "",
-    review: "You cooked 🔥",
-    clientImage: "/clientReview/ayan.png",
-  },
-  {
-    name: "James Charles",
-    subs: "",
-    review: "I’m great!!! I came across your thumbnail work, you’re amazing!",
-    clientImage: "/clientReview/james.png",
-  },
-  {
-    name: "Shawn Herrin",
-    subs: "",
-    review: "Bottom right. No idea why, the contrast grabs my eye. When I expanded the image they were all so good. But at small size the white shirt got me.",
-    clientImage: "/clientReview/shawn.png",
-  }, 
-  {
-    name: "Saima",
-    subs: "",
-    review: "Awesome, after seeing this Jimmy would be motivated to go the gym",
-    clientImage: "/clientReview/saima.png",
-  },
-  {
-    name: "Justin Pugh",
-    subs: "",
-    review: "Super easy to work with and crazy talented. Thumbnail BOSS always takes our ideas and levels them up!",
-    clientImage: "/clientReview/justinpugh.png",
-  },
-  {
-    name: "Blue Thunder",
-    subs: "",
-    review: "Brilliant thumbnail designer—his work is clean, impactful, and optimized for maximum engagement!",
-    clientImage: "/clientReview/blueThunder.png",
-  },
-  {
-    name: "ZHC Crafts",
-    subs: "",
-    review: "Fast, reliable, and always on point! ",
-    clientImage: "/clientReview/zhcCrafts.png",
-  },
-  {
-    name: "ZHC",
-    subs: "",
-    review: "Great guy, even better thumbnails.",
-    clientImage: "/clientReview/zhc.png",
-  },
-  {
-    name: "Kamahley",
-    subs: "",
-    review: "Extremely communicative and always focused on making the best possible design.",
-    clientImage: "/clientReview/Kamahley.png",
-  },
-];
-
+export const clientReviewFirstCol : IClientReview[] = clientsReviews.slice(0,4)
+export const clientReviewSecondCol = clientsReviews.slice(4,8)
+export const clientReviewThirdCol = clientsReviews.slice(8)
 
 export const clientsWork = [
   {
