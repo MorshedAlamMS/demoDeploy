c6553282b2c6e1d358d2b40fe09502dc084c38ac
sajibms <shahadath.sajib.ms@gmail.com>
2025-03-10 15:35:28 +0600

    feat: loaded data of client marquee fromclient content file

---
+++ app/ContentData/MyClients/MyClientsContent.ts
@@ -0,0 +1,23 @@
+export const clientsName = [
+  "Client One",
+  "Client Two",
+  "Client Three",
+  "Client Four",
+];
+
+export const clientsSubs = [
+  "7.6M subs",
+  "24.5M subs",
+  "854K subs",
+  "18.2K subs",
+];
+
+
+// * image will be uploaded in /public/MyClients/
+// * make sure the image name is same as the name in the array
+export const clientsImg = [
+  "client1.png",
+  "client2.png",
+  "client3.png",
+  "client4.png",
+];
--- app/components/section/home/Clients.tsx
+++ app/components/section/home/Clients.tsx
@@ -1,15 +1,21 @@
+import { nanoid } from "nanoid";
 import ClientCard from "~/components/card/ClientCard";
 import { Marquee } from "~/components/ui/marquee";
-import { clients } from "~/constants";
+import { clientsImg, clientsName, clientsSubs } from "~/ContentData";
 
+const clients = clientsName.map((name, index) => ({
+  name,
+  subscribers: clientsSubs[index],
+  imgName: clientsImg[index],
+}));
 
 const Clients = () => {
   return (
     <section>
       <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
         <Marquee pauseOnHover className="[--duration:20s]">
-          {clients.map((client, index) => (
-            <ClientCard key={index} {...client} />
+          {clients.map((client) => (
+            <ClientCard key={nanoid()} {...client} />
           ))}
         </Marquee>
         <div className="pointer-events-none absolute inset-y-0 left-0 w-1/4 bg-gradient-to-r from-background"></div>
